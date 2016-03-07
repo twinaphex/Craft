fb555e0ed0f8be8b887951e8c3b7f529ddd702f5
twinaphex <libretro@gmail.com>
2016-03-07 09:26:54 +0100

    Add ifdefs to GL code inside util.c

--- src/util.c
+++ src/util.c
@@ -38,30 +38,39 @@ char *load_file(const char *path) {
 }
 
 GLuint gen_buffer(GLsizei size, GLfloat *data) {
+#if defined(HAVE_OPENGL) || defined(HAVE_OPENGLES)
     GLuint buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
     glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     return buffer;
+#endif
 }
 
 void del_buffer(GLuint buffer) {
+#if defined(HAVE_OPENGL) || defined(HAVE_OPENGLES)
     glDeleteBuffers(1, &buffer);
+#endif
 }
 
 GLfloat *malloc_faces(int components, int faces) {
-    return malloc(sizeof(GLfloat) * 6 * components * faces);
+#if defined(HAVE_OPENGL) || defined(HAVE_OPENGLES)
+    return malloc(sizeof(GLfloat) *  6 * components * faces);
+#endif
 }
 
 GLuint gen_faces(int components, int faces, GLfloat *data) {
+#if defined(HAVE_OPENGL) || defined(HAVE_OPENGLES)
     GLuint buffer = gen_buffer(
         sizeof(GLfloat) * 6 * components * faces, data);
     free(data);
     return buffer;
+#endif
 }
 
 GLuint make_shader(GLenum type, const char *source) {
+#if defined(HAVE_OPENGL) || defined(HAVE_OPENGLES)
     GLuint shader = glCreateShader(type);
     glShaderSource(shader, 1, &source, NULL);
     glCompileShader(shader);
@@ -76,6 +85,7 @@ GLuint make_shader(GLenum type, const char *source) {
         free(info);
     }
     return shader;
+#endif
 }
 
 GLuint load_shader(GLenum type, const char *path) {
@@ -86,6 +96,7 @@ GLuint load_shader(GLenum type, const char *path) {
 }
 
 GLuint make_program(GLuint shader1, GLuint shader2) {
+#if defined(HAVE_OPENGL) || defined(HAVE_OPENGLES)
     GLuint program = glCreateProgram();
     glAttachShader(program, shader1);
     glAttachShader(program, shader2);
@@ -105,13 +116,16 @@ GLuint make_program(GLuint shader1, GLuint shader2) {
     glDeleteShader(shader1);
     glDeleteShader(shader2);
     return program;
+#endif
 }
 
 GLuint load_program(const char *path1, const char *path2) {
+#if defined(HAVE_OPENGL) || defined(HAVE_OPENGLES)
     GLuint shader1 = load_shader(GL_VERTEX_SHADER, path1);
     GLuint shader2 = load_shader(GL_FRAGMENT_SHADER, path2);
     GLuint program = make_program(shader1, shader2);
     return program;
+#endif
 }
 
 void flip_image_vertical(
@@ -137,8 +151,10 @@ void load_png_texture(const char *file_name) {
         fprintf(stderr, "error %u: %s\n", error, lodepng_error_text(error));
     }
     flip_image_vertical(data, width, height);
+#if defined(HAVE_OPENGL) || defined(HAVE_OPENGLES)
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA,
         GL_UNSIGNED_BYTE, data);
+#endif
     free(data);
 }
 
