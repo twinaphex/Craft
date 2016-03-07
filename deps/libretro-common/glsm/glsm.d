af18534888efd796fbadf2ece46b8f71354758f1
twinaphex <libretro@gmail.com>
2016-03-07 02:41:58 +0100

    (Step 3) Add glsm/glsym and compile it in

---
+++ deps/libretro-common/glsm/glsm.d
@@ -0,0 +1,8 @@
+deps/libretro-common/glsm/glsm.o: deps/libretro-common/glsm/glsm.c \
+ deps/libretro-common/include/glsym/glsym.h \
+ deps/libretro-common/include/glsym/rglgen.h \
+ deps/libretro-common/include/glsym/rglgen_headers.h \
+ deps/libretro-common/include/glsym/glsym_gl.h \
+ deps/libretro-common/include/glsm/glsm.h \
+ deps/libretro-common/include/boolean.h src/libretro.h \
+ deps/libretro-common/include/glsym/rglgen_headers.h
