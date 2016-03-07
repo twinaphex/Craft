fefff7105ff29d7b6dea8db0d3a0d0f0819c743a
twinaphex <libretro@gmail.com>
2016-03-07 04:38:39 +0100

    Move files to libretro/, and create duplicate of main (main_libretro.c)

--- deps/libretro-common/glsm/glsm.d
+++
@@ -1,8 +0,0 @@
-deps/libretro-common/glsm/glsm.o: deps/libretro-common/glsm/glsm.c \
- deps/libretro-common/include/glsym/glsym.h \
- deps/libretro-common/include/glsym/rglgen.h \
- deps/libretro-common/include/glsym/rglgen_headers.h \
- deps/libretro-common/include/glsym/glsym_gl.h \
- deps/libretro-common/include/glsm/glsm.h \
- deps/libretro-common/include/boolean.h src/libretro.h \
- deps/libretro-common/include/glsym/rglgen_headers.h
