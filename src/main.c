2b781e7946a12c61ee2ec075a12db242cfa348be
rz5 <razevok@gmail.com>
2016-03-11 01:37:43 +0000

    Attempt to correct a bad #if, which caused a missing closing quotes. Also tweaked the formatting of the shader source code.

--- src/main.c
+++ src/main.c
@@ -3406,7 +3406,7 @@ static const char *text_fragment_shader[] = {
    "  else {",
    "    color.a = max(color.a, 0.4);",
    "  }",
-   "gl_FragColor = color;",
+   "  gl_FragColor = color;",
    "}",
 };
 
@@ -3500,47 +3500,47 @@ static const char *water_fragment_shader[] = {
 };
 
 static const char *block_fragment_shader[] = {
-   "#version " GLSL_VERSION "\n"
+    "#version " GLSL_VERSION "\n"
 #if defined(HAVE_OPENGLES2)
     "precision lowp float; \n"
 #else
-#define highp \n"
+    "precision highp float; \n"
 #endif
-   "uniform sampler2D sampler;",
-   "uniform sampler2D sky_sampler;",
-   "uniform float timer;",
-   "uniform float daylight;",
-   "uniform int ortho;",
-   "varying vec2 fragment_uv;",
-   "varying float fragment_ao;",
-   "varying float fragment_light;",
-   "varying float fog_factor;",
-   "varying float fog_height;",
-   "varying float diffuse;",
-   "const float pi = 3.14159265;",
-   "void main() {",
+    "uniform sampler2D sampler;",
+    "uniform sampler2D sky_sampler;",
+    "uniform float timer;",
+    "uniform float daylight;",
+    "uniform int ortho;",
+    "varying vec2 fragment_uv;",
+    "varying float fragment_ao;",
+    "varying float fragment_light;",
+    "varying float fog_factor;",
+    "varying float fog_height;",
+    "varying float diffuse;",
+    "const float pi = 3.14159265;",
+    "void main() {",
     "  vec3 color = vec3(texture2D(sampler, fragment_uv));",
     "  if (color == vec3(1.0, 0.0, 1.0)) {",
     "    discard;",
+    "  }",
+    "  bool cloud = color == vec3(1.0, 1.0, 1.0);",
+    "  if (cloud && bool(ortho)) {",
+    "    discard;",
+    "  }",
+    "  float df = cloud ? 1.0 - diffuse * 0.2 : diffuse;",
+    "  float ao = cloud ? 1.0 - (1.0 - fragment_ao) * 0.2 : fragment_ao;",
+    "  ao = min(1.0, ao + fragment_light);",
+    "  df = min(1.0, df + fragment_light);",
+    "  float value = min(1.0, daylight + fragment_light);",
+    "  vec3 light_color = vec3(value * 0.3 + 0.2);",
+    "  vec3 ambient = vec3(value * 0.3 + 0.2) + \n"
+    "      vec3(sin(pi*daylight)/2, sin(pi*daylight)/4, 0.0);",
+    "  vec3 light = ambient + light_color * df;",
+    "  color = clamp(color * light * ao, vec3(0.0), vec3(1.0));",
+    "  vec3 sky_color = vec3(texture2D(sky_sampler, vec2(timer, fog_height)));",
+    "  color = mix(color, sky_color, fog_factor);",
+    "  gl_FragColor = vec4(color, 1.0);",
     "}",
-    "bool cloud = color == vec3(1.0, 1.0, 1.0);",
-    "if (cloud && bool(ortho)) {",
-    "  discard;",
-    "}",
-    "float df = cloud ? 1.0 - diffuse * 0.2 : diffuse;",
-    "float ao = cloud ? 1.0 - (1.0 - fragment_ao) * 0.2 : fragment_ao;",
-    "ao = min(1.0, ao + fragment_light);",
-    "df = min(1.0, df + fragment_light);",
-    "float value = min(1.0, daylight + fragment_light);",
-    "vec3 light_color = vec3(value * 0.3 + 0.2);",
-    "vec3 ambient = vec3(value * 0.3 + 0.2) + \n"
-       "vec3(sin(pi*daylight)/2, sin(pi*daylight)/4, 0.0);",
-    "vec3 light = ambient + light_color * df;",
-    "color = clamp(color * light * ao, vec3(0.0), vec3(1.0));",
-    "vec3 sky_color = vec3(texture2D(sky_sampler, vec2(timer, fog_height)));",
-    "color = mix(color, sky_color, fog_factor);",
-    "gl_FragColor = vec4(color, 1.0);",
-   "}",
 };
 
 static const char *block_vertex_shader[] = {
@@ -3576,7 +3576,7 @@ static const char *block_vertex_shader[] = {
    "    float dy = position.y - camera.y;",
    "    float dx = distance(position.xz, camera.xz);",
    "    fog_height = (atan(dy, dx) + pi / 2) / pi;",
-   "}",
+   "  }",
    "}",
 };
 
