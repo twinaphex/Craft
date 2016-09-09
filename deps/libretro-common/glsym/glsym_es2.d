682739755ddff613ded043840dc87df87f0fe898
twinaphex <libretro@gmail.com>
2016-09-09 16:11:12 +0200

    Udpate

--- deps/libretro-common/glsym/glsym_es2.d
+++
@@ -1,6 +0,0 @@
-deps/libretro-common/glsym/glsym_es2.o: \
-  deps/libretro-common/glsym/glsym_es2.c \
-  deps/libretro-common/include/glsym/glsym.h \
-  deps/libretro-common/include/glsym/rglgen.h \
-  deps/libretro-common/include/glsym/rglgen_headers.h \
-  deps/libretro-common/include/glsym/glsym_es2.h
