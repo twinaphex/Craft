b26439886522e9555631041c04f3eaeaaad3e353
twinaphex <libretro@gmail.com>
2017-07-06 09:10:22 +0200

    Updates

--- src/map.c
+++ src/map.c
@@ -43,50 +43,53 @@ void map_copy(Map *dst, Map *src) {
     memcpy(dst->data, src->data, (dst->mask + 1) * sizeof(MapEntry));
 }
 
-int map_set(Map *map, int x, int y, int z, int w) {
-    unsigned int index = hash(x, y, z) & map->mask;
-    x -= map->dx;
-    y -= map->dy;
-    z -= map->dz;
-    MapEntry *entry = map->data + index;
-    int overwrite = 0;
-    while (!EMPTY_ENTRY(entry)) {
-        if (entry->e.x == x && entry->e.y == y && entry->e.z == z) {
-            overwrite = 1;
-            break;
-        }
-        index = (index + 1) & map->mask;
-        entry = map->data + index;
-    }
-    if (overwrite) {
-        if (entry->e.w != w) {
-            entry->e.w = w;
-            return 1;
-        }
-    }
-    else if (w) {
-        entry->e.x = x;
-        entry->e.y = y;
-        entry->e.z = z;
-        entry->e.w = w;
-        map->size++;
-        if (map->size * 2 > map->mask) {
-            map_grow(map);
-        }
-        return 1;
-    }
-    return 0;
+int map_set(Map *map, int x, int y, int z, int w)
+{
+   MapEntry *entry;
+   int overwrite = 0;
+   unsigned int index = hash(x, y, z) & map->mask;
+   x -= map->dx;
+   y -= map->dy;
+   z -= map->dz;
+   entry = map->data + index;
+   while (!EMPTY_ENTRY(entry)) {
+      if (entry->e.x == x && entry->e.y == y && entry->e.z == z) {
+         overwrite = 1;
+         break;
+      }
+      index = (index + 1) & map->mask;
+      entry = map->data + index;
+   }
+   if (overwrite) {
+      if (entry->e.w != w) {
+         entry->e.w = w;
+         return 1;
+      }
+   }
+   else if (w) {
+      entry->e.x = x;
+      entry->e.y = y;
+      entry->e.z = z;
+      entry->e.w = w;
+      map->size++;
+      if (map->size * 2 > map->mask) {
+         map_grow(map);
+      }
+      return 1;
+   }
+   return 0;
 }
 
 int map_get(Map *map, int x, int y, int z) {
     unsigned int index = hash(x, y, z) & map->mask;
+    MapEntry *entry;
     x -= map->dx;
     y -= map->dy;
     z -= map->dz;
     if (x < 0 || x > MAX_BLOCK_HEIGHT) return 0;
     if (y < 0 || y > MAX_BLOCK_HEIGHT) return 0;
     if (z < 0 || z > MAX_BLOCK_HEIGHT) return 0;
-    MapEntry *entry = map->data + index;
+    entry = map->data + index;
     while (!EMPTY_ENTRY(entry)) {
         if (entry->e.x == x && entry->e.y == y && entry->e.z == z) {
             return entry->e.w;
